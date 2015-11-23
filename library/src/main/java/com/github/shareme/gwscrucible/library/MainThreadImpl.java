e5d0525c55b415a888733d4b889d384e22b610e3
Fred Grott <fred.grott@gmail.com>
2015-11-23 09:33:53 -0600

    added library code

---
+++ library/src/main/java/com/github/shareme/gwscrucible/library/MainThreadImpl.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente GÃ³mez SÃ¡nchez.
+ * Modifications Copyright(C) 2015 Fred Grott(GrottWorkShop)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.shareme.gwscrucible.library;
+
+import android.os.Handler;
+import android.os.Looper;
+
+/**
+ * MainThread implementation based on a Handler instantiated over the main looper obtained from
+ * Created by fgrott on 11/23/2015.
+ */
+@SuppressWarnings("unused")
+class MainThreadImpl implements MainThread {
+
+    private Handler handler;
+
+    MainThreadImpl() {
+        this.handler = new Handler(Looper.getMainLooper());
+    }
+
+    public void post(Runnable runnable) {
+        handler.post(runnable);
+    }
+}
